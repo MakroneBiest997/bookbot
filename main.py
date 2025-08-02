6b40b9b605682a1166ed5f010f68d3ddd7439652
MakroneBiest997 <marcus.hamm.mh@gmail.com>
2025-08-02 22:47:09 +0200

    bookbot working but code still ugly

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("greetings boots")
+from stats import count_words
+from stats import count_each_character
+from stats import unsorted_dictionary_list
+from stats import get_sort_key
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        book_text = f.read()
+    return book_text
+
+def print_ordered_char_list(dic_list):
+    return 0
+
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    book_dictionary = count_each_character(book_text)
+    
+    unordered_dic_list = unsorted_dictionary_list(book_dictionary)
+    unordered_dic_list.sort(reverse=True, key=get_sort_key)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")  
+    print("--------- Character Count -------")
+    
+    for item in unordered_dic_list:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    
+    print("============= END ===============")
+
+main()
+
---
+++ stats.py
@@ -0,0 +1,31 @@
+book_path = "books/frankenstein.txt"
+
+def count_words(text):
+    word_list = text.split()
+    word_count = len(word_list)
+    return word_count
+
+
+
+def count_each_character(text):
+
+    lower_case_text = text.lower()
+    character_list = list(lower_case_text)
+    character_dic = {}
+    
+    for character in character_list:
+        if character not in character_dic:
+            character_dic[character] = 1
+        elif character in character_dic:
+             character_dic[character] += 1
+    return character_dic
+
+def unsorted_dictionary_list(dictonary):
+    dic_list = []
+    for key, value in dictonary.items():
+        list_value = {"char" : key , "num" : value}
+        dic_list.append(list_value)
+    return dic_list
+
+def get_sort_key(dictionary):
+    return dictionary["num"]
